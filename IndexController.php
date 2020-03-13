1d51771ce6c0d0b162abd54d2ec6e5a705f342f5
luochuan2008 <156356969@qq.com>
2020-03-14 00:21:53 +0800

    ceshi

---
+++ IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WIN10
+ * Date: 2020/3/14
+ * Time: 0:04
+ */
+
+class IndexController
+{
+    public function index(){
+        return "hello !";
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WIN10
+ * Date: 2020/3/14
+ * Time: 0:06
+ */
